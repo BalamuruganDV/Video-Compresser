9f71395395bd8ddce37710d7ab4c62293c3b2dcd
Tanujairam <88611197+Tanujairam123@users.noreply.github.com>
2022-01-04 11:05:40 +0530

    Update localisation.py

--- bot/localisation.py
+++ bot/localisation.py
@@ -47,7 +47,7 @@ class Localisation:
     
     HELP_MESSAGE = get_config(
         "STRINGS_HELP_MESSAGE",
-        'Hi I am Video Compressor Bot \n\n1. Send me your telegram big video file \n2. Reply to the file with: `/compress 50` \n\n『💥<a href="https://t.me/REX_Bots_Support"><b>𝕽𝖊𝖝 𝕾𝖚𝖕𝖕𝖔𝖗𝖙</b></a> 💥』/n/n'
+        'Hi ,I am Video Compressor Bot \n\n1. Send me your telegram big video file \n2. Reply to the file with: `/compress 50` \n\n『💥<a href="https://t.me/REX_Bots_Support"><b>𝕽𝖊𝖝 𝕾𝖚𝖕𝖕𝖔𝖗𝖙</b></a> 💥』/n/n'
     )
     WRONG_MESSAGE = get_config(
         "STRINGS_WRONG_MESSAGE",
